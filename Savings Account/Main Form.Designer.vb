6c2dc1293e1d4f9c4fd21a836df6dd7a81bc4413
pngebura <32974270+pngebura@users.noreply.github.com>
2017-11-11 15:27:51 -0500

    Final app

---
+++ Savings Account/Main Form.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(MainForm))
+        Me.principalLabel = New System.Windows.Forms.Label()
+        Me.principalTextBox = New System.Windows.Forms.TextBox()
+        Me.calculateButton = New System.Windows.Forms.Button()
+        Me.exitButton = New System.Windows.Forms.Button()
+        Me.balanceListBox = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'principalLabel
+        '
+        Me.principalLabel.AutoSize = True
+        Me.principalLabel.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.principalLabel.Location = New System.Drawing.Point(76, 25)
+        Me.principalLabel.Name = "principalLabel"
+        Me.principalLabel.Size = New System.Drawing.Size(79, 23)
+        Me.principalLabel.TabIndex = 0
+        Me.principalLabel.Text = "&Principal:"
+        Me.principalLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'principalTextBox
+        '
+        Me.principalTextBox.Location = New System.Drawing.Point(156, 21)
+        Me.principalTextBox.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.principalTextBox.Name = "principalTextBox"
+        Me.principalTextBox.Size = New System.Drawing.Size(79, 29)
+        Me.principalTextBox.TabIndex = 1
+        Me.principalTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'calculateButton
+        '
+        Me.calculateButton.Location = New System.Drawing.Point(25, 85)
+        Me.calculateButton.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.calculateButton.Name = "calculateButton"
+        Me.calculateButton.Size = New System.Drawing.Size(120, 52)
+        Me.calculateButton.TabIndex = 2
+        Me.calculateButton.Text = "&Calculate"
+        Me.calculateButton.UseVisualStyleBackColor = True
+        '
+        'exitButton
+        '
+        Me.exitButton.Location = New System.Drawing.Point(165, 85)
+        Me.exitButton.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.exitButton.Name = "exitButton"
+        Me.exitButton.Size = New System.Drawing.Size(120, 52)
+        Me.exitButton.TabIndex = 3
+        Me.exitButton.Text = "E&xit"
+        Me.exitButton.UseVisualStyleBackColor = True
+        '
+        'balanceListBox
+        '
+        Me.balanceListBox.FormattingEnabled = True
+        Me.balanceListBox.ItemHeight = 21
+        Me.balanceListBox.Location = New System.Drawing.Point(13, 169)
+        Me.balanceListBox.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.balanceListBox.Name = "balanceListBox"
+        Me.balanceListBox.Size = New System.Drawing.Size(285, 340)
+        Me.balanceListBox.TabIndex = 4
+        Me.balanceListBox.TabStop = False
+        '
+        'MainForm
+        '
+        Me.AcceptButton = Me.calculateButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 21.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(311, 519)
+        Me.Controls.Add(Me.balanceListBox)
+        Me.Controls.Add(Me.exitButton)
+        Me.Controls.Add(Me.calculateButton)
+        Me.Controls.Add(Me.principalTextBox)
+        Me.Controls.Add(Me.principalLabel)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "MainForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Savings Account"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents principalLabel As Label
+    Friend WithEvents principalTextBox As TextBox
+    Friend WithEvents calculateButton As Button
+    Friend WithEvents exitButton As Button
+    Friend WithEvents balanceListBox As ListBox
+End Class
---
+++ Savings Account/Main Form.vb
@@ -0,0 +1,65 @@
+﻿' Project name:     Savings Account
+' Project purpose:  Shows balance at 3 different rates over 5 years
+' Created by:       Peter Gebura on 10/26/17
+
+Option Explicit On
+Option Infer Off
+Option Strict On
+
+Public Class MainForm
+    Private Sub exitButton_Click(sender As Object, e As EventArgs) Handles exitButton.Click
+        ' Exit button close when clicked
+
+        Me.Close()
+    End Sub
+
+    Private Sub calculateButton_Click(sender As Object, e As EventArgs) Handles calculateButton.Click
+        ' Declare variables
+
+        Dim beginPrincipal As Double
+        Dim principal As Double
+        Dim increase As Double
+
+        ' Assign input to variables
+        Double.TryParse(principalTextBox.Text, beginPrincipal)
+        balanceListBox.Items.Clear()
+        balanceListBox.Items.Add("Rate     Year     Balance")
+
+        ' Calculate and display yearly balances
+        For rate As Double = 0.02 To 0.04 Step 0.01
+            principal = beginPrincipal
+            balanceListBox.Items.Add(rate.ToString("P0"))
+            For year As Integer = 1 To 5
+                increase = principal * rate
+                principal += increase
+                balanceListBox.Items.Add("            " & year.ToString & "         " & principal.ToString("C0"))
+            Next year
+        Next rate
+
+        ' Sends focus to textbox
+        principalTextBox.Focus()
+        principalTextBox.SelectAll()
+    End Sub
+
+    Private Sub principalTextBox_KeyPress(sender As Object, e As KeyPressEventArgs) Handles principalTextBox.KeyPress
+        ' Accepts only numbers and backspace key
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso
+            e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub principalTextBox_Enter(sender As Object, e As EventArgs) Handles principalTextBox.Enter
+        ' Selects existing text when textbox receives focus
+
+        principalTextBox.Focus()
+        principalTextBox.SelectAll()
+    End Sub
+
+    Private Sub principalTextBox_TextChanged(sender As Object, e As EventArgs) Handles principalTextBox.TextChanged
+        ' Clears the listbox
+
+        balanceListBox.Items.Clear()
+    End Sub
+End Class
